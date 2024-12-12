bd5619c47bf21d54e920de9e1c5d2e9bfe18c42b
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-12 19:45:46 -0300

    sabrina- commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,20 @@
-import { SignInButton } from "@clerk/clerk-react"
+import {  useClerk, UserButton, SignInButton } from "@clerk/clerk-react"
 
 const Home = () => {
+  const { redirectToSignIn } = useClerk();
+
+  const handleLogin = () => {
+    redirectToSignIn();
+  };
+
   return (
     <div>
         <h1>Sing in Whith this Button</h1>
-        <SignInButton/>
+          <button onClick={handleLogin}>Logar</button>
+          <SignInButton />
+
+          {/* Depois que o usuario for logado mostra a foto fo usuario */}
+          <UserButton />
     </div>
   )
 }
